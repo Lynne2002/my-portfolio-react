0d01bfb0c79138f88f7b7fc49a743656e464a5c0
Lynne Chepkwony <lynnebebet2@gmail.com>
2024-01-29 22:46:05 +0300

    feat: add typed_script component to Home.js
    And change contact link

--- my-portfolio-react/src/components/Home.js
+++ my-portfolio-react/src/components/Home.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import Typed_script from './Typed_script'
 
 function Home() {
   return (
@@ -9,21 +11,21 @@ function Home() {
     <div className="text">
       <p className="text_p1">Hello, I am</p>
       <h1 className="title">Lynne Chebet</h1>
-      <div className="text-3">And I'm a 
-        <span className="typing">
+      <div className="text-3">And I'm a <span className="typing">
         </span>
+        <Typed_script/>
     </div>
     <br/>
       <div className="btn-container">
         <button
           className="btn btn-color-2"
-          onclick="window.open('./assets/my_CV.pdf')"
+  
         >
           My CV
         </button>
-        <button className="btn btn-color-1" onclick="location.href='#contact'">
-          My contacts
-        </button>
+        <Link to = "../contact"><button className="btn btn-color-1">
+          My contacts </button></Link>
+       
       </div>
     </div>
 </section>
