6484e4ef1e4541425fba80a814671689cec6771d
Lynne Chepkwony <lynnebebet2@gmail.com>
2024-01-29 16:38:41 +0300

    chore: change class to className

--- my-portfolio-react/src/components/Projects.js
+++ my-portfolio-react/src/components/Projects.js
@@ -2,54 +2,54 @@ import React from 'react'
 
 function Projects() {
   return (
-    <section class="portfolio-section sec-padding" id="portfolio">
-    <div class="about-container">
-        <div class="about-row">
-            <div class="section-title">
+    <section className="portfolio-section sec-padding" id="portfolio">
+    <div className="about-container">
+        <div className="about-row">
+            <div className="section-title">
               <h2>Recent Work</h2>
             </div>
         </div>
-        <div class="about-row">
+        <div className="about-row">
         
            
-            <div class="portfolio-item">
-                <div class="portfolio-item-thumbnail">
+            <div className="portfolio-item">
+                <div className="portfolio-item-thumbnail">
                     <img  src="assets/upload3.png" alt="portfolio-item-thumbnail"/>
                 </div>
-                <h3 class="portfolio-item-title">Hotel Website</h3>
+                <h3 className="portfolio-item-title">Hotel Website</h3>
                
                 
             </div>
              
-            <div class="portfolio-item">
-                <div class="portfolio-item-thumbnail">
+            <div className="portfolio-item">
+                <div className="portfolio-item-thumbnail">
                     <img src="./assets/upload6.png" alt="portfolio item thumb"/>
                 </div>
-                <h3 class="portfolio-item-title">Shoe Website</h3>
+                <h3 className="portfolio-item-title">Shoe Website</h3>
                 
                 
             </div>
             
-            <div class="portfolio-item">
-                <div class="portfolio-item-thumbnail">
+            <div className="portfolio-item">
+                <div className="portfolio-item-thumbnail">
                     <img src="./assets/upload1.png" alt="portfolio item thumb"/>
                 </div>
-                <h3 class="portfolio-item-title">Real Estate Website</h3> 
+                <h3 className="portfolio-item-title">Real Estate Website</h3> 
             </div>
              
-            <div class="portfolio-item">
-                <div class="portfolio-item-thumbnail">
+            <div className="portfolio-item">
+                <div className="portfolio-item-thumbnail">
                     <img src="./assets/upload4.png" alt="portfolio item thumb"/>
                 </div>
-                <h3 class="portfolio-item-title">Akan Name Generator</h3>
+                <h3 className="portfolio-item-title">Akan Name Generator</h3>
             </div>
              
         
-            <div class="portfolio-item">
-                <div class="portfolio-item-thumbnail">
+            <div className="portfolio-item">
+                <div className="portfolio-item-thumbnail">
                     <img src="./assets/upload5.png" alt="portfolio item thumb"/>
                 </div>
-                <h3 class="portfolio-item-title">Crime Reporting Website</h3>
+                <h3 className="portfolio-item-title">Crime Reporting Website</h3>
             </div>
           
         </div>
