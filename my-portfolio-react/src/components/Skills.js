e3317b16a90bdeaa548dce343a96db8982bca3ec
Lynne Chepkwony <lynnebebet2@gmail.com>
2024-01-29 15:17:27 +0300

    feat(Skills): add skills component

---
+++ my-portfolio-react/src/components/Skills.js
@@ -0,0 +1,163 @@
+import React from 'react'
+
+function Skills() {
+  return (
+
+<section class="my_skills" id="skills">
+  <div class="section-title">
+    <h2>My skills</h2>
+  </div>
+  <div class="div-box">
+    <div class="skills-content">
+      <h3>Frontend</h3>
+      <div class="ind-content">
+        <span><i class="fa-brands fa-html5"></i>   HTML</span>
+        <span><i class="fa-brands fa-css3-alt"></i>CSS</span>
+        <span><i class="fa-brands fa-js"></i>JavaScript</span>
+        <span><i class="fa-brands fa-bootstrap"></i>Bootstrap</span>
+      </div>
+    </div>
+    <div class="skills-content">
+      <h3>Backend</h3>
+      <div class="ind-content">
+        <span><i class="fa-brands fa-python"></i>Python</span>
+        <span><i class="fa-brands fa-java"></i>Java</span>
+        <span><i class="fa-brands fa-php"></i>PHP</span>
+        <span><i class="fa-brands fa-node"></i>Node</span>
+      </div>
+    </div>
+    <div class="skills-content">
+      <h3>UI/UX Design</h3>
+      <div class="ind-content">
+        <span><i class="fa-brands fa-figma"></i>Figma</span>
+      </div>
+    </div>
+
+  </div>
+  <div class="about-skills">
+    <div class="skill-item">
+      <div class="skill-header">
+        <h6 class="skill-title">Node</h6>
+        <div class="skill-percentage">
+          <p><span class="counter">20</span>%</p>
+        </div>
+      </div>
+      <div class="skill-bar">
+        <div class="bar-inner">
+          <div class="progress-line" data-progress="20%"></div>
+        </div>
+      </div>
+    </div>
+    <div class="skill-item">
+      <div class="skill-header">
+        <h6 class="skill-title">Bootstrap</h6>
+        <div class="skill-percentage">
+          <p><span class="counter">30</span>%</p>
+        </div>
+      </div>
+      <div class="skill-bar">
+        <div class="bar-inner">
+          <div class="progress-line" data-progress="30%"></div>
+        </div>
+      </div>
+    </div>
+    <div class="skill-item">
+      <div class="skill-header">
+        <h6 class="skill-title">Java</h6>
+        <div class="skill-percentage">
+          <p><span class="counter">30</span>%</p>
+        </div>
+      </div>
+      <div class="skill-bar">
+        <div class="bar-inner">
+          <div class="progress-line" data-progress="30%"></div>
+        </div>
+      </div>
+    </div>
+    <div class="skill-item">
+      <div class="skill-header">
+        <h6 class="skill-title">JavaScript</h6>
+        <div class="skill-percentage">
+          <p><span class="counter">40</span>%</p>
+        </div>
+      </div>
+      <div class="skill-bar">
+        <div class="bar-inner">
+          <div class="progress-line" data-progress="40%"></div>
+        </div>
+      </div>
+    </div>
+    <div class="skill-item">
+      <div class="skill-header">
+        <h6 class="skill-title">Figma</h6>
+        <div class="skill-percentage">
+          <p><span class="counter">40</span>%</p>
+        </div>
+      </div>
+      <div class="skill-bar">
+        <div class="bar-inner">
+          <div class="progress-line" data-progress="40%"></div>
+        </div>
+      </div>
+    </div>
+    <div class="skill-item">
+      <div class="skill-header">
+        <h6 class="skill-title">Python</h6>
+        <div class="skill-percentage">
+          <p><span class="counter">50</span>%</p>
+        </div>
+      </div>
+      <div class="skill-bar">
+        <div class="bar-inner">
+          <div class="progress-line" data-progress="50%"></div>
+        </div>
+      </div>
+    </div>
+    <div class="skill-item">
+      <div class="skill-header">
+        <h6 class="skill-title">CSS</h6>
+        <div class="skill-percentage">
+          <p><span class="counter">50</span>%</p>
+        </div>
+      </div>
+      <div class="skill-bar">
+        <div class="bar-inner">
+          <div class="progress-line" data-progress="50%"></div>
+        </div>
+      </div>
+    </div>
+    <div class="skill-item">
+      <div class="skill-header">
+        <h6 class="skill-title">PHP</h6>
+        <div class="skill-percentage">
+          <p><span class="counter">50</span>%</p>
+        </div>
+      </div>
+      <div class="skill-bar">
+        <div class="bar-inner">
+          <div class="progress-line" data-progress="50%"></div>
+        </div>
+      </div>
+    </div>
+    <div class="skill-item">
+      <div class="skill-header">
+        <h6 class="skill-title">HTML</h6>
+        <div class="skill-percentage">
+          <p><span class="counter">70</span>%</p>
+        </div>
+      </div>
+      <div class="skill-bar">
+        <div class="bar-inner">
+          <div class="progress-line" data-progress="70%"></div>
+        </div>
+      </div>
+    </div>
+    
+  </div>
+
+
+</section>
+  )
+}
+
+export default Skills
