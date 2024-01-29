ea94c20523db1ca842960a42639da962fad7e721
Lynne Chepkwony <lynnebebet2@gmail.com>
2024-01-29 19:24:48 +0300

    feat: add routing for components

--- my-portfolio-react/src/App.js
+++ my-portfolio-react/src/App.js
@@ -6,18 +6,23 @@ import Home from './components/Home';
 import Projects from './components/Projects';
 import Sec_spacing from './components/Sec_spacing';
 import Skills from './components/Skills';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 
 function App() {
   return (
     <>
+    <Router>
     <Header/>
-    <Home/>
-    <About/>
-    <Sec_spacing/>
-    <Skills/>
-    <Sec_spacing/>
-    <Projects/>
-    <Contact/>
+    <Routes>
+      <Route path='home' Component={Home}/>
+      <Route path='/about' Component={About}/>
+      <Route Component={Sec_spacing}/>
+      <Route path='/skills' Component={Skills}/>
+      <Route Component={Sec_spacing}/>
+      <Route path='/portfolio' Component={Projects}/>
+      <Route path='/contact' Component={Contact}/>
+    </Routes>
+    </Router>
     </>
   );
 }
