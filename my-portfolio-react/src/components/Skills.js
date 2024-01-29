4c7653f64bc436a5fa3f5a50d21ccbe70134a6e3
Lynne Chepkwony <lynnebebet2@gmail.com>
2024-01-29 22:50:19 +0300

    feat(Skills): add react font awesome icons

--- my-portfolio-react/src/components/Skills.js
+++ my-portfolio-react/src/components/Skills.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import {FaHtml5,FaCss3Alt, FaJs, FaBootstrap, FaPython, FaNode, FaJava, FaPhp, FaFigma } from "react-icons/fa";
 function Skills() {
   return (
 
@@ -11,25 +11,25 @@ function Skills() {
     <div className="skills-content">
       <h3>Frontend</h3>
       <div className="ind-content">
-        <span><i className="fa-brands fa-html5"></i>   HTML</span>
-        <span><i className="fa-brands fa-css3-alt"></i>CSS</span>
-        <span><i className="fa-brands fa-js"></i>JavaScript</span>
-        <span><i className="fa-brands fa-bootstrap"></i>Bootstrap</span>
+        <span><i className="fa-brands fa-html5"><FaHtml5/> </i>  HTML</span>
+        <span><i className="fa-brands fa-css3-alt"><FaCss3Alt/></i>CSS</span>
+        <span><i className="fa-brands fa-js"><FaJs/></i>JavaScript</span>
+        <span><i className="fa-brands fa-bootstrap"><FaBootstrap/></i>Bootstrap</span>
       </div>
     </div>
     <div className="skills-content">
       <h3>Backend</h3>
       <div className="ind-content">
-        <span><i className="fa-brands fa-python"></i>Python</span>
-        <span><i className="fa-brands fa-java"></i>Java</span>
-        <span><i className="fa-brands fa-php"></i>PHP</span>
-        <span><i className="fa-brands fa-node"></i>Node</span>
+        <span><i className="fa-brands fa-python"><FaPython/></i>Python</span>
+        <span><i className="fa-brands fa-java"><FaJava/></i>Java</span>
+        <span><i className="fa-brands fa-php"><FaPhp/></i>PHP</span>
+        <span><i className="fa-brands fa-node"><FaNode/></i>Node</span>
       </div>
     </div>
     <div className="skills-content">
       <h3>UI/UX Design</h3>
       <div className="ind-content">
-        <span><i className="fa-brands fa-figma"></i>Figma</span>
+        <span><i className="fa-brands fa-figma"><FaFigma/></i>Figma</span>
       </div>
     </div>
 
