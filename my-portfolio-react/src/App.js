bf467ce5de3d58a0e2ef5c5fc7e0ee78377dd690
Lynne Chepkwony <lynnebebet2@gmail.com>
2024-01-29 16:54:05 +0300

    feat: add sec_spacing between sections

--- my-portfolio-react/src/App.js
+++ my-portfolio-react/src/App.js
@@ -4,6 +4,7 @@ import Contact from './components/Contact';
 import Header from './components/Header';
 import Home from './components/Home';
 import Projects from './components/Projects';
+import Sec_spacing from './components/Sec_spacing';
 import Skills from './components/Skills';
 
 function App() {
@@ -12,7 +13,9 @@ function App() {
     <Header/>
     <Home/>
     <About/>
+    <Sec_spacing/>
     <Skills/>
+    <Sec_spacing/>
     <Projects/>
     <Contact/>
     </>
